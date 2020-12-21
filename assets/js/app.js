264cdeaf9bf65a82776d0b52ea2e8aa6aff5730b
Seds <seds.admin@gmail.com>
2020-12-21 23:43:26 +0100

    Add FadeOut effect to Flash message

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,9 @@ import '../css/app.scss';
 // js add on
 $(function() {
     
-    // ...
+    if( $('#flash_message').length )
+    {
+        $('#flash_message').delay(3000).fadeOut();
+    }
     
 });
