6633a6b14f5fbdb0b4247a56567817b4e447ee8e
SeDs <seds.admin@gmail.com>
2021-06-12 23:18:12 +0200

    Some adjustments..

--- assets/back/js/back_app.js
+++ assets/back/js/back_app.js
@@ -7,3 +7,15 @@ global.$ = global.jQuery = $;
 require('startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js');
 require('startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min.js');
 require('startbootstrap-sb-admin-2/js/sb-admin-2.min.js');
+    
+$(document).ready(function(){
+    
+    // UTF8 decode
+    $('body').find('.decode').each(function(){
+        $(this).html($(this).html($(this).html()).text()).text()
+    })
+    
+    // Bootstrap tooltip
+    $('body').find('[data-toggle="tooltip"]').tooltip()
+    
+})
--- assets/front/js/front_app.js
+++ assets/front/js/front_app.js
@@ -8,19 +8,20 @@ global.$ = global.jQuery = $;
 require('bootstrap');
 
 // js add on
-$(function() {
+$(document).ready(function(){
     
+    // UTF8 decode
+    $('body').find('.decode').each(function(){
+        $(this).html($(this).html($(this).html()).text()).text()
+    })
+    
+    // Bootstrap tooltip
+    $('body').find('[data-toggle="tooltip"]').tooltip()
+    
+    // Flash message display
     if( $('#flash_message').length )
     {
         $('#flash_message').delay(3000).fadeOut();
     }
     
-    // Init Bootstrap tooltip
-    $('[data-toggle="tooltip"]').tooltip()
-    
-    // Decode UTF8
-    $('body').find('.decode').each(function(){
-        $(this).html($(this).html($(this).html()).text()).text()
-    })
-    
-});
+})
