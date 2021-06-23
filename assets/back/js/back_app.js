7adcc5aa950ba21b52a2609039bb07115569fe65
SeDs <seds.admin@gmail.com>
2021-06-23 23:04:03 +0200

    Update styles

--- assets/back/js/back_app.js
+++ assets/back/js/back_app.js
@@ -10,6 +10,13 @@ require('startbootstrap-sb-admin-2/js/sb-admin-2.min.js');
     
 $(document).ready(function(){
     
+    // Responsive Navbar collapse menu
+    if ($(window).width() < 768) $('.sidebar .collapse').removeClass('show');
+
+    $(window).resize(function () {
+        if ($(window).width() >= 768) $('.sidebar .nav-item.active .collapse').collapse('show');
+    });
+        
     // UTF8 decode
     $('body').find('.decode').each(function(){
         $(this).html($(this).html($(this).html()).text()).text()
@@ -17,5 +24,11 @@ $(document).ready(function(){
     
     // Bootstrap tooltip
     $('body').find('[data-toggle="tooltip"]').tooltip()
+    
+    // Flash message display
+    if( $('#flash_message').length )
+    {
+        $('#flash_message').delay(3000).fadeOut()
+    }
     
 })
