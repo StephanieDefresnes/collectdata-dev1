0a96d05e3a4ed9866a3373bf267c97359b9346c1
SeDs <seds.admin@gmail.com>
2021-06-23 22:59:26 +0200

    Structure changes

---
+++ assets/back/js/user_read_app.js
@@ -0,0 +1,14 @@
+// css
+import '../scss/user_read_app.scss';
+
+// js
+$(document).ready(function(){
+    
+    $('.btn-delete').click(function(){
+        var title = $(this).attr('data-title');
+        var path = $(this).attr('data-path');
+        $('#form_back_user_delete').attr('action', path);
+        $('#modal_body_title').html(translations['deleteConfirm']+" : <strong>"+title+"</strong>");
+    });
+    
+});
