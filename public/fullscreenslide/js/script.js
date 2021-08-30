37b3656d8fd31372a37faad27839ce2ac360e5aa
SeDs <seds.admin@gmail.com>
2021-08-30 21:33:44 +0200

    Add footer to FrontOffice, Templates reorganization & Slideshow adjustment

---
+++ public/fullscreenslide/js/script.js
@@ -0,0 +1,26 @@
+$(function() {
+   
+    let windowHeight = $(window).height()
+    let lastHeight = $('body').height()
+
+    if (windowHeight > lastHeight) {
+        $('#footerEnd').height(windowHeight - lastHeight)
+    }
+
+    let footerEnd = setInterval(function() {
+        let newHeight = $('body').height()
+        let contentHeight = newHeight - $('#footerEnd').height()
+
+        if (lastHeight != newHeight) {
+            lastHeight = newHeight;
+            lastWidth = newWidth;
+        }
+        if (windowHeight > contentHeight) {
+            $('#footerEnd').height(windowHeight - contentHeight)
+        } else {
+            $('#footerEnd').height(0)
+            clearInterval(footerEnd)
+        }
+    }, 100)
+    
+})
