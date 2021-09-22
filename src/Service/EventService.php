97c2fc840f401308957fac4f16edd12acfa61ab6
SeDs <seds.admin@gmail.com>
2021-09-22 17:06:51 +0200

    Remove useless functions

--- src/Service/EventService.php
+++ src/Service/EventService.php
@@ -47,19 +47,5 @@ class EventService {
             ]);
         return $qb->getQuery()->getResult();
     }
-        
-    /**
-     * @return []   Returns Category data validated
-     */
-    public function getDataById($event_id)
-    {
-        return $this->em->createQueryBuilder()
-                ->from(Event::class,'e')
-                ->select('e.title, e.validated')
-                ->where('e.id = ?1')
-                ->setParameter(1, $event_id)
-                ->getQuery()
-                ->getOneOrNullResult();
-    }
     
 }
