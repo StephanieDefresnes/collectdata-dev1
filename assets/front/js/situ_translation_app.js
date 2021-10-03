eaf570ce87324bdc89aef7cffd83fa8c4618f92f
SeDs <seds.admin@gmail.com>
2021-10-03 22:15:00 +0200

    Update Situ translation

--- assets/front/js/situ_translation_app.js
+++ assets/front/js/situ_translation_app.js
@@ -3,13 +3,13 @@ import '../scss/situ_translation_app.scss'
 
 // Load translation lang & show fields
 function loadTranslation(langId) {
-    $('#create_situ_form_lang').val(langId).trigger('change')
+    $('#situ_form_lang').val(langId).trigger('change')
          .parent().find('.select2-selection__rendered').addClass('selection-on')
     
-    $('#translateTo').text($('#select2-create_situ_form_lang-container').html())
+    $('#translateTo').text($('#select2-situ_form_lang-container').html())
  
     let dataExist = setInterval(function() {
-        if ($('#create_situ_form_event option').length) {
+        if ($('#situ_form_event option').length) {
             $('#categoryLevel1, #categoryLevel2').removeClass('d-none')
             $('#loader').removeClass('d-block')
             clearInterval(dataExist)
