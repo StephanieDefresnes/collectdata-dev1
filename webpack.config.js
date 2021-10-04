c1875f07f56120daa3f1adac55c853a67ca5f539
SeDs <seds.admin@gmail.com>
2021-10-04 17:50:15 +0200

    Little fix

--- webpack.config.js
+++ webpack.config.js
@@ -84,6 +84,7 @@ Encore
         pattern: /\.(png|jpg|jpeg)$/
     })
     .addEntry('back_app', './assets/back/js/back_app.js')
+    .addEntry('index_app', './assets/back/js/index_app.js')
     .addEntry('lang_translation_clean_app', './assets/back/js/lang_translation_clean_app.js')
     .addEntry('lang_translation_create_app', './assets/back/js/lang_translation_create_app.js')
     .addEntry('lang_translation_site_app', './assets/back/js/lang_translation_site_app.js')
