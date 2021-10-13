cabb42484d4c9481d9f166157a189168ffd26d48
SeDs <seds.admin@gmail.com>
2021-10-13 20:56:15 +0200

    Add jQ confirm to user delete

--- assets/back/js/user_read_app.js
+++ assets/back/js/user_read_app.js
@@ -4,11 +4,27 @@ import '../scss/user_read_app.scss';
 // js
 $(document).ready(function(){
     
-    $('.btn-delete').click(function(){
-        var title = $(this).attr('data-title');
-        var path = $(this).attr('data-path');
-        $('#form_back_user_delete').attr('action', path);
-        $('#modal_body_title').html(translations['deleteConfirm']+" : <strong>"+title+"</strong>");
-    });
-    
+    $('.userDelete').confirm({
+        animation: 'scale',
+        closeAnimation: 'scale',
+        animateFromElement: false,
+        columnClass: 'col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2',
+        type: 'red',
+        typeAnimated: true,
+        title: translations["user"],
+        content: '<p class="line-11">'+ translations["confirm"] +'</p>'
+                +'<p class="mb-2 text-center font-weight-bold text-danger">'+ translations["warning"] +'</p>',
+        buttons: {
+            cancel: {
+                text: translations['no'],
+            },
+            formSubmit: {
+                text: translations['yes'],
+                btnClass: 'btn-red',
+                action: function () {
+                    location.href = this.$target.attr('href');
+                }
+            }
+        },
+    })
 });
