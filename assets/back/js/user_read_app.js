33f66cad61ad9dd0918920e8d8b8ba80923db63f
SeDs <seds.admin@gmail.com>
2021-10-14 15:53:29 +0200

    Read user improvement

--- assets/back/js/user_read_app.js
+++ assets/back/js/user_read_app.js
@@ -1,17 +1,15 @@
-// css
-import '../scss/user_read_app.scss';
-
-// js
-$(document).ready(function(){
+function deleteUser(element) {
+    let user = element.attr('data-user')
+    let url = element.attr('data-url')
     
-    $('.userDelete').confirm({
+    $.confirm({
         animation: 'scale',
         closeAnimation: 'scale',
         animateFromElement: false,
         columnClass: 'col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2',
         type: 'red',
         typeAnimated: true,
-        title: translations["user"],
+        title: translations["user"] + user,
         content: '<p class="line-11">'+ translations["confirm"] +'</p>'
                 +'<p class="mb-2 text-center font-weight-bold text-danger">'+ translations["warning"] +'</p>',
         buttons: {
@@ -22,9 +20,16 @@ $(document).ready(function(){
                 text: translations['yes'],
                 btnClass: 'btn-red',
                 action: function () {
-                    location.href = this.$target.attr('href');
+                    location.href = url
                 }
             }
         },
+    })
+}
+
+$(function(){
+    
+    $('.userDelete').click(function() {
+        deleteUser($(this))
     })
 });
---
+++ assets/back/js/user_search_app.js
@@ -0,0 +1,54 @@
+// css
+import '../scss/table_app.scss';
+
+// js
+require('datatables.net/js/jquery.dataTables.min.js');
+require('datatables.net-bs4/js/dataTables.bootstrap4.min.js');
+const lang = require('../../datatables.json')
+
+$(function() {
+    
+    if ($('#dataTable-list tbody tr').length == 0) $('#loader').hide()
+    
+    /**
+     * Translations list
+     */
+    // Datatables configuration
+    var table = $('#dataTable-list').DataTable({
+        language: {
+            url: '//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/'
+                    + lang[$('html').attr('lang')] +'.json',
+        },
+        dom: '<"d-flex justify-content-between row mb-2"<"#length.col-md-5"l><"#search.col-auto"f>>'
+                +'<"table-responsive border"t>'
+                +'<"row"<"#info.col-md-6 small"i><"#pagination.col-md-6 mt-3"p>>',
+        "columnDefs": [{
+            orderable: false,
+            targets: 'no-sort'
+        }],
+        "fnDrawCallback": function(oSettings) {
+            $('#dataTable-list_filter input').addClass('search')
+            
+            // Hide length select & pagination if only one page
+            if ($('#dataTable-list').dataTable().fnSettings().fnRecordsTotal() <= 10) {
+               $('#length, #pagination .dataTables_paginate').hide()
+               $('#search .dataTables_filter').addClass('text-left')
+            
+            }
+            $('#loader').hide()
+        }
+    })
+
+    // Reset search filter
+    $('#list').on('keyup paste', 'input.search', function() {
+        $(this).parent().find('.clean-search').remove('.clean-search')
+        if ($(this).val() != '') {
+            $(this).parent().append('<span class="clean-search"><i class="fas fa-times"></i></span>')
+        }
+    })
+    $('#list').on('click', '.clean-search', function() {
+        table.search('').columns().search('').draw();
+        $(this).remove()
+    })
+    
+});
