de7bd9a828d4e9441a52404591292842cfdb0016
SeDs <seds.admin@gmail.com>
2021-11-04 03:43:50 +0100

    Add jQuery confirm

--- assets/back/js/lang_translation_site_app.js
+++ assets/back/js/lang_translation_site_app.js
@@ -6,143 +6,76 @@ require('datatables.net/js/jquery.dataTables.min.js');
 require('datatables.net-bs4/js/dataTables.bootstrap4.min.js');
 const lang = require('../../datatables.json')
 
-// Datatables configuration
-function setDatatable(table) {
-    $('#'+ table).DataTable({
+$(function() {
+    
+    if ($('#dataTable-list tbody tr').length == 0) $('#loader').hide()
+    
+    /**
+     * Translations list
+     */
+    // Datatables configuration
+    var table = $('#dataTable-list').DataTable({
         language: {
             url: '//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/'
                     + lang[$('html').attr('lang')] +'.json',
         },
-        dom: '<"d-flex justify-content-between row mb-2"<"#'+ table +'_length.col-md-5"l><"#'+ table +'_search.col-auto"f>>'
+        dom: '<"d-flex justify-content-between row mb-2"<"#length.col-md-5"l><"#search.col-auto"f>>'
                 +'<"table-responsive border"t>'
-                +'<"row"<"#'+ table +'_info.col-md-6 small"i><"#'+ table +'_pagination.col-md-6 mt-3"p>>',
+                +'<"row"<"#info.col-md-6 small"i><"#pagination.col-md-6 mt-3"p>>',
         "columnDefs": [{
             orderable: false,
-            targets: 0
+            targets: 'no-sort'
         }],
-        "order": [[ 1, 'asc' ]],
-        "fnDrawCallback": function(oSettings) {            
-            // Add class to load reset button search
-            $('#'+ table +'_filter input').addClass('search')
+        "fnDrawCallback": function(oSettings) {
+            $('#dataTable-list_filter input').addClass('search')
             
             // Hide length select & pagination if only one page
-            if ($('#'+ table).dataTable().fnSettings().fnRecordsTotal() <= 10) {
-               $('#'+ table +'_length, #'+ table +'_pagination .dataTables_paginate').hide()
-               $('#'+ table +'_search .dataTables_filter').addClass('text-left')
-            }
+            if ($('#dataTable-list').dataTable().fnSettings().fnRecordsTotal() <= 10) {
+               $('#length, #pagination .dataTables_paginate').hide()
+               $('#search .dataTables_filter').addClass('text-left')
             
+            }
             $('#loader').hide()
+        },
+        initComplete: function () {
+            this.api().columns([2]).every( function () {
+                var column = this;
+                let selectContent = '<select class="form-control form-control-sm">'
+                                    +'<option value="">'+ translations['select'] +'</option></select>'
+                var select = $(selectContent)
+                    .appendTo( $('#filter').empty() )
+                    .on('change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option class="text-uppercase" value="'+d+'">'+d+'</option>' )
+                } );
+            } );
         }
     })
-}
 
-// Reset search filter
-function resetFilter(table) {  
-    let oTable = $('#'+ table).DataTable()
-    
-    $('#'+ table + '-list').on('keyup paste', 'input.search', function() {
+    // Reset search filter
+    $('#list').on('keyup paste', 'input.search', function() {
         $(this).parent().find('.clean-search').remove('.clean-search')
         if ($(this).val() != '') {
-            $(this).parent().append('<span class="clean-search small pointer"><i class="fas fa-times"></i></span>')
+            $(this).parent().append('<span class="clean-search"><i class="fas fa-times"></i></span>')
         }
     })
-    $('#'+ table + '-list').on('click', '.clean-search', function() {
-        oTable.search('').columns().search('').draw();
+    $('#list').on('click', '.clean-search', function() {
+        table.search('').columns().search('').draw();
         $(this).remove()
     })
-}
-
-// Select entries
-function selectEntries(checkBox) {
-    $('#'+ checkBox).change(function() {
-        let listType = $(this).parents('table').attr('data-list')
-        
-        if (this.checked) {
-            $(this).parents('table').find('.select').each(function() {
-                this.checked=true;
-            });
-            // Show actions
-            $('#'+ listType +'Actions').removeClass('d-none').animate({opacity: 1}, 450)
-            
-            $(this).parents('.tab-pane').find('#create')
-                    .addClass('d-none').css('opacity', 0)
-            
-        } else {
-            $(this).parents('table').find('.select').each(function() {
-                this.checked=false;
-            });
-            // Hide actions row
-            $('#'+ listType +'Actions').addClass('d-none').css('opacity', 0)
-            
-            $(this).parents('.tab-pane').find('#create')
-                    .removeClass('d-none').animate({opacity: 1}, 450)
-        }
-    });
-}
-
-function checkActionResult(name) {
-    if ($('form[name="translation_batch_'+ name +'"] > .alert').length == 1) {
-        // Open actions menu if rows are checked
-        $('#'+ name +' input[type="checkbox"]').each(function() {
-            if ($(this).prop('checked') == true && $('#'+ name +'Actions').hasClass('d-none'))
-                $('#'+ name +'Actions').removeClass('d-none')
-        })
-    } else {
-        // Uncheck input
-        $('#'+ name +' input[type="checkbox"]').each(function() {
-            $(this).prop('checked', false)
-        })
-    }
-}
-
-$(function() {
     
-    if ($('#siteEmpty').length == 1 && $('#contributorEmpty').length == 1)
-        $('#loader').hide()
-    
-    // Switch navtab if error on contributor form
-    if ($('form[name="translation_batch_contributor"] > .alert').length == 1) {
-        $('#contrib-tab').tab('show')
-    }
-    
-    // Check result to toggle actions menu & checkboxes
-    checkActionResult('site')
-    checkActionResult('site')
-    
-    // Datatables configuration
-    setDatatable('dataTable-site')
-    setDatatable('dataTable-contributor')
-    resetFilter('dataTable-site')
-    resetFilter('dataTable-contributor')
-    
-    // Check boxes management
-    selectEntries('selectAllSite')
-    selectEntries('selectAllContributor')
-    
-    // Toggle actions menu
-    $('.form-check-input').change(function() {
-        let listType = $(this).parents('table').attr('data-list')
-        
-        if (this.checked && $('#'+ listType +'Actions').hasClass('d-none')) {
-            
-            $('#'+ listType +'Actions').removeClass('d-none').animate({opacity: 1}, 450)
-            
-            $(this).parents('.tab-pane').find('#create')
-                    .addClass('d-none').css('opacity', 0)
-            
-        } else if (!this.checked && !$('#'+ listType +'Actions').hasClass('d-none')) {
-            var check = 0;
-            $(this).parents('table').find('.select').each(function() {
-                if (this.checked) check += 1
-            });
-            if(check == 0) $('#'+ listType +'Actions').addClass('d-none').css('opacity', 0)
-            
-            $(this).parents('.tab-pane').find('#create')
-                    .removeClass('d-none').animate({opacity: 1}, 450)
-        }
-    })
-    
-    // Modal
+    /*
+     * Modal create translation
+     */
     $('#createTranslation').click(function() {
         let referentId = $('#siteEnvironnement').val()
         let langId = $('#siteLang').val()
---
+++ assets/back/js/lang_translations_app.js
@@ -0,0 +1,141 @@
+// Get data to set confirm
+function getConfirm() {
+    let name, lang, btnColor, titleClass, title, content, confirm = []
+    let value = $('#translation_batch_action').val()
+    
+    // Styles
+    if (value === 'delete') {
+        btnColor = 'btn-danger'
+        titleClass = ' text-danger'
+    } else {
+        btnColor = 'btn-primary'
+        titleClass = ' text-primary'
+    }
+
+    let checked = $('tbody input:checkbox:checked').length
+
+    // Multiple rows selected
+    if (checked > 1) {
+        // Set title
+        title = '<div class="d-sm-flex justify-content-start'+ titleClass +'">'
+                +'<span class="'+ titleClass +'">'+ translations[value +'-titles'] + '</span>'
+                +'</div>'
+        
+        // Set content
+        content = '<ul>'
+        $('tbody input:checkbox:checked').each(function() {
+            name = $('tbody input:checkbox:checked').parents('tr').find('td.name').text()
+            content += '<li class="d-sm-flex justify-content-start"><span class="px-2">'+ name +'</span>'
+            
+            lang = $('tbody input:checkbox:checked').parents('tr').find('td.lang').text()
+            if (lang)
+                content += '<span class="text-uppercase">'+ lang +'</span>'
+                
+            content += '</li>'
+        })
+        content += '</ul>'
+                +'<p class="line-11">'+ translations[value +'-confirms'] +'</p>'
+        
+        if (value === 'delete')
+            content += '<p class="line-11">'+ translations['warning'] +'</p>'
+
+    }
+    // Single row selected
+    else {
+        name = $('tbody input:checkbox:checked').parents('tr').find('td.name').text()
+        lang = $('tbody input:checkbox:checked').parents('tr').find('td.lang').text()
+        
+        // Set title
+        title = '<div class="d-sm-flex justify-content-start'+ titleClass +'">'
+                +'<span>'+ translations[value +'-title'] + '</span>'
+                + '<span class="px-2">'+ name + '</span>'
+                +'<span class="text-uppercase">'+ lang +'</span></div>'
+        
+        // Set content
+        content = '<p class="line-11">'+ translations[value +'-confirm'] +'</p>'        
+    }
+    
+    if (value === 'delete')
+        content += '<p class="text-danger text-center">'+ translations['warning'] +'</p>'
+    
+    confirm['titleClass'] = titleClass
+    confirm['btnColor'] = btnColor
+    confirm['title'] = title
+    confirm['content'] = content
+    
+    return confirm 
+}
+
+function submitConfirm(button) {    
+    button.confirm({
+        animation: 'scale',
+        closeAnimation: 'scale',
+        animateFromElement: false,
+        columnClass: 'col-lg-6 col-md-8 col-sm-10 mx-auto',
+        typeAnimated: true,
+        title: function(){
+            return getConfirm()['title']
+        },
+        content: function(){
+            return getConfirm()['content']
+        },
+        buttons: {
+            cancel: {
+                text: translations['no'],
+                action: function () {}
+            },
+            confirm: {
+                text: translations['yes'],
+                btnClass: function(){
+                    return getConfirm()['btnColor']
+                },
+                action: function () {
+                    $('form').submit()
+                }
+            }
+        },
+    });    
+}
+
+$(function(){
+    
+    // Entries selection
+    $('#select_all').change(function() {
+        if (this.checked) {
+            $(".select").each(function() {
+                this.checked=true;
+            });
+            // Show actions row
+            $('#formActions').removeClass('d-none').animate({opacity: 1}, 450)
+        } else {
+            $('.select').each(function() {
+                this.checked=false;
+            });
+            // Hide actions row
+            $('#formActions').addClass('d-none').css('opacity', 0)
+        }
+    });
+
+    // Toggle actions menu
+    $('.form-check-input').change(function() {
+        if (this.checked && $('#formActions').hasClass('d-none')) {
+            $('#formActions').removeClass('d-none').animate({opacity: 1}, 450)
+        } else if (!this.checked && !$('#formActions').hasClass('d-none')) {
+            var check = 0;
+            $('.select').each(function() {
+                if (this.checked) check += 1
+            });
+            if(check == 0) $('#formActions').addClass('d-none').css('opacity', 0)
+        }
+    })
+    
+    // Show actions menu if rows are checked
+    $('.form-check-input').each(function() {
+        if ($(this).prop('checked') == true && $('#formActions').hasClass('d-none'))
+            $('#formActions').removeClass('d-none')
+    })
+    
+    // Confirm before submit
+    submitConfirm($('#translation_batch_submit'))
+    
+});
