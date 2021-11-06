ac1357e06cec894711bc56159a75582f7b9ffc00
SeDs <seds.admin@gmail.com>
2021-11-06 10:44:25 +0100

    Fix path manager

--- src/Form/Back/User/UserBatchType.php
+++ src/Form/Back/User/UserBatchType.php
@@ -3,7 +3,7 @@
 namespace App\Form\Back\User;
 
 use App\Entity\User;
-use App\Manager\UserManager;
+use App\Manager\Back\UserManager;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\AbstractType;
