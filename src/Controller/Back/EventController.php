7f061ed7904483de5b13f19e9a371c058a7444a7
SeDs <seds.admin@gmail.com>
2021-11-07 22:06:00 +0100

    Some minor fixes

--- src/Controller/Back/EventController.php
+++ src/Controller/Back/EventController.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
