201ecb67db7d00783ab4d1df969ca05f46c41490
SeDs <seds.admin@gmail.com>
2021-11-08 20:55:00 +0100

    Add slug property

--- src/Entity/Situ.php
+++ src/Entity/Situ.php
@@ -6,6 +6,7 @@ use App\Repository\SituRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass=SituRepository::class)
@@ -23,6 +24,12 @@ class Situ
      * @ORM\Column(type="string", length=190)
      */
     private $title;
+
+    /**
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(type="string", length=190)
+     */
+    private $slug;
 
     /**
      * @ORM\Column(type="text")
@@ -130,6 +137,11 @@ class Situ
         $this->title = $title;
 
         return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
     }
 
     public function getDescription(): ?string
